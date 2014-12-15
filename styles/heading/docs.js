bc525790cb8c84b4e5d99d85d5e2942e74c5f5c4
Justin Joseph <jjoseph@signal.co>
2014-12-15 15:39:23 -0600

    Changed headings format

--- styles/heading/docs.js
+++ styles/heading/docs.js
@@ -3,22 +3,7 @@ module.exports = {
   state:  'Alpha',
   examples: [
     {
-      html: '<h1>Heading 1</h1>'
-    },
-    {
-      html: '<h2>Heading 2</h2>'
-    },
-    {
-      html: '<h3>Heading 3</h3>'
-    },
-    {
-      html: '<h4>Heading 4</h4>'
-    },
-    {
-      html: '<h5>Heading 5</h5>'
-    },
-    {
-      html: '<h6>Heading 6</h6>'
+      html: '<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><h4>Heading 4</h4><h5>Heading 5</h5><h6>Heading 6</h6>'
     }
   ]
 };
