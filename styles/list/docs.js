9468262608022c6c4435853454332b4df1cf2a27
Justin Joseph <jjoseph@signal.co>
2014-12-18 10:51:39 -0600

    Added ordered and definition lists

--- styles/list/docs.js
+++ styles/list/docs.js
@@ -5,6 +5,14 @@ module.exports = {
     {
     	title: 'Unordered Lists',
       html: '<ul>\r\n<li>Item 1</li>\n\n<li>Item 2</li>\n\n<li>Item 2</li>\n\n</ul>'
+    },
+    {
+    	title: 'Ordered Lists',
+      html: '<ol>\r\n<li>Item 1</li>\n\n<li>Item 2</li>\n\n<li>Item 2</li>\n\n</ol>'
+    },
+    {
+    	title: 'Definition Lists',
+      html: '<dl>\r\n<di>Item 1</di>\n\n<dd>something interesting</dd>\n\n<di>Item 2</di><dd>something delightful</dd>\n\n</dl>'
     }
   ]
 };
