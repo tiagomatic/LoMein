a9a59944b671770c5f92ed5e186be5822478104f
Brian Peacock <bpeacock@signal.co>
2014-12-18 23:41:12 -0600

    Fixed navigation back to home page

--- site/main.js
+++ site/main.js
@@ -55,7 +55,7 @@ $(function() {
 
       // Highlight the link
       $asideLinks.removeClass('active');
-      $('aside a[href="#'+hash+'"]').addClass('active');
+      $('aside a[href$="#'+hash+'"]').addClass('active');
     };
 
     $window.scroll(onScroll);
