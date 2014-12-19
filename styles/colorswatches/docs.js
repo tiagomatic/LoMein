d9235db562a4f5cd9f277608e29710767e301c5e
Brian Peacock <bpeacock@signal.co>
2014-12-19 13:10:23 -0600

    Changed color spacing

--- styles/colorswatches/docs.js
+++ styles/colorswatches/docs.js
@@ -5,37 +5,38 @@ module.exports = {
     { 
       noMarkup: true,
       html: '\
-<div class="swatch-container">\n\
-  <div class="signal-blue"><p>#00adef</p></div>\n\
-  <div class="dark-signal-blue"><p>#00688f</p></div>\n\
-</div>\n\
-<div class="swatch-container">\n\
-  <div class="signal-red"><p>#ea1c2c</p></div>\n\
-  <div class="dark-signal-red"><p>#b81623</p></div>\n\
-</div>\n\
-<div class="swatch-container">\n\
-  <div class="teal"><p>#00a19a</p></div>\n\
-  <div class="dark-teal"><p>#004b48</p></div>\n\
-</div>\n\
-<div class="swatch-container">\n\
-  <div class="green"><p>#00a10b</p></div>\n\
-  <div class="dark-green"><p>#006107</p></div>\n\
-</div>\n\
-<div class="swatch-container">\n\
-  <div class="blue-lagoon"><p>#004d6b</p></div>\n\
-  <div class="dark-blue-lagoon"><p>#00364a</p></div>\n\
-</div>\n\
-<div class="swatch-container">\n\
-  <div class="lemon-curd"><p>#ffc556</p></div>\n\
-  <div class="dark-lemon-curd"><p>#edb750</p></div>\n\
-</div>\n\
-<div class="swatch-container">\n\
-  <div class="grey"><p>#cccccc</p></div>\n\
-  <div class="dark-grey"><p>#333333</p></div>\n\
-</div>\n\
-<div class="swatch-container">\n\
-  <div class="white"><p>#ffffff</p></div>\n\
-  <div class="light-grey"><p>#dfdfdf</p></div>\n\
+<div class="swatch-container">\
+  <div class="signal-blue"><p>#00adef</p></div>\
+  <div class="dark-signal-blue"><p>#00688f</p></div>\
+</div>\
+<div class="swatch-container">\
+  <div class="signal-red"><p>#ea1c2c</p></div>\
+  <div class="dark-signal-red"><p>#b81623</p></div>\
+</div>\
+<div class="swatch-container">\
+  <div class="grey"><p>#cccccc</p></div>\
+  <div class="dark-grey"><p>#333333</p></div>\
+</div>\
+<div class="swatch-container">\
+  <div class="white"><p>#ffffff</p></div>\
+  <div class="light-grey"><p>#dfdfdf</p></div>\
+</div>\
+<br>\
+<div class="swatch-container">\
+  <div class="teal"><p>#00a19a</p></div>\
+  <div class="dark-teal"><p>#004b48</p></div>\
+</div>\
+<div class="swatch-container">\
+  <div class="green"><p>#00a10b</p></div>\
+  <div class="dark-green"><p>#006107</p></div>\
+</div>\
+<div class="swatch-container">\
+  <div class="blue-lagoon"><p>#004d6b</p></div>\
+  <div class="dark-blue-lagoon"><p>#00364a</p></div>\
+</div>\
+<div class="swatch-container">\
+  <div class="lemon-curd"><p>#ffc556</p></div>\
+  <div class="dark-lemon-curd"><p>#edb750</p></div>\
 </div>'
     }
   ]
