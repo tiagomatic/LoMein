248bc9f8cce12bee8b1917067a4c6ad3784fad03
Brian Peacock <bpeacock@signal.co>
2014-12-18 23:31:15 -0600

    debug

--- main.js
+++ main.js
@@ -1,80 +1,82 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 $(function() {
-	var $window = $(window);
+  var $window = $(window);
 
-	// Navigation Positioning
-	var $aside 	= $('aside'),
-			headerH = 70,
-			lastScroll;
+  // Navigation Positioning
+  var $aside  = $('aside'),
+      headerH = 70,
+      lastScroll;
 
-	var onScroll = function() {
-		var scrollTop = $window.scrollTop();
+  var onScroll = function() {
+    var scrollTop = $window.scrollTop();
 
-		if(scrollTop < 0) {
-			$aside.css('top', headerH);
-		}
-		else if(scrollTop < headerH) {
-			$aside.css('top', headerH - scrollTop);
-		}
-		else {
-			$aside.css('top', 0);
-		}
-	};
+    if(scrollTop < 0) {
+      $aside.css('top', headerH);
+    }
+    else if(scrollTop < headerH) {
+      $aside.css('top', headerH - scrollTop);
+    }
+    else {
+      $aside.css('top', 0);
+    }
+  };
 
-	$window.scroll(onScroll);
-	onScroll();
+  $window.scroll(onScroll);
+  onScroll();
 
-	// Navigation Sizing
-	var asideHeight = $aside.height();
+  // Navigation Sizing
+  var asideHeight = $aside.height();
 
-	var onResize = function() {
-		$aside.outerHeight($window.height());
-	};
+  var onResize = function() {
+    $aside.outerHeight($window.height());
+  };
 
-	$window.resize(onResize);
-	onResize();
+  $window.resize(onResize);
+  onResize();
 
-	// Active Link Selection
-	var path 				= window.location.pathname,
-			$headings   = $('h2[id]'),
-			$asideLinks = $('aside a');
+  // Active Link Selection
+  var path        = window.location.pathname,
+      $headings   = $('h2[id]'),
+      $asideLinks = $('aside a');
 
-	if(path === '/') {
-		var onScroll = function() {
-			// Get the current heading
-			var hash,
-					scrollTop = $window.scrollTop();
+  console.log(path);
 
-			$headings.each(function() {
-				var $this = $(this),
-						top 	= $this.position().top;
+  if(path === '/') {
+    var onScroll = function() {
+      // Get the current heading
+      var hash,
+          scrollTop = $window.scrollTop();
 
-				if(scrollTop > top) {
-					hash = $(this).attr('id');
-				}
-			});
+      $headings.each(function() {
+        var $this = $(this),
+            top   = $this.position().top;
 
-			// Highlight the link
-			$asideLinks.removeClass('active');
-			$('aside a[href="#'+hash+'"]').addClass('active');
-		};
+        if(scrollTop > top) {
+          hash = $(this).attr('id');
+        }
+      });
 
-		$window.scroll(onScroll);
+      // Highlight the link
+      $asideLinks.removeClass('active');
+      $('aside a[href="#'+hash+'"]').addClass('active');
+    };
 
-		onScroll();
-	}
-	else {
-		var regex = new RegExp('^[^#].*'+path+'?$');
+    $window.scroll(onScroll);
 
-		$('aside a').each(function() {
-			var $this = $(this),
-					href 	= $this.attr('href');
+    onScroll();
+  }
+  else {
+    var regex = new RegExp('^[^#].*'+path+'?$');
 
-			if(regex.test(href)) {
-				$this.addClass('active');
-			}
-		});
-	}
+    $('aside a').each(function() {
+      var $this = $(this),
+          href  = $this.attr('href');
+
+      if(regex.test(href)) {
+        $this.addClass('active');
+      }
+    });
+  }
 });
 
 },{}]},{},[1])
