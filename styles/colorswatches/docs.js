b0503801fb821b5fef30ae4f93600409653ecda5
Justin Joseph <jjoseph@signal.co>
2014-12-19 10:17:25 -0600

    Updated html format

--- styles/colorswatches/docs.js
+++ styles/colorswatches/docs.js
@@ -4,41 +4,38 @@ module.exports = {
   examples: [
     {
       html: '\
-      <div class="swatch-container">\n\n\
-      <div id="signal-blue"><p>#00adef</p></div>\n\n\
-      <div id="dark-signal-blue"><p>#00688f</p></div>\n\n\
-      </div>\n\n\
-	  <div class="swatch-container">\n\n\
-      <div id="signal-red"><p>#ea1c2c</p></div>\n\n\
-      <div id="dark-signal-red"><p>#b81623</p></div>\n\n\
-      </div>\n\n\
-	  <div class="swatch-container">\n\n\
-      <div id="teal"><p>#00a19a</p></div>\n\n\
-      <div id="dark-teal"><p>#004b48</p></div>\n\n\
-      </div>\n\n\
-	  <div class="swatch-container">\n\n\
-      <div id="green"><p>#00a10b</p></div>\n\n\
-      <div id="dark-green"><p>#006107</p></div>\n\n\
-      </div>\n\n\
-	  <div class="swatch-container">\n\n\
-      <div id="blue-lagoon"><p>#004d6b</p></div>\n\n\
-      <div id="dark-blue-lagoon"><p>#00364a</p></div>\n\n\
-      </div>\n\n\
-	  <div class="swatch-container">\n\n\
-      <div id="lemon-curd"><p>#ffc556</p></div>\n\n\
-      <div id="dark-lemon-curd"><p>#edb750</p></div>\n\n\
-      </div>\n\n\
-	  <div class="swatch-container">\n\n\
-      <div id="grey"><p>#cccccc</p></div>\n\n\
-      <div id="dark-grey"><p>#333333</p></div>\n\n\
-      </div>\n\n\
-	  <div class="swatch-container">\n\n\
-      <div id="white"><p>#ffffff</p></div>\n\n\
-      <div id="light-grey"><p>#dfdfdf</p></div>\n\n\
+      <div class="swatch-container">\n\
+        <div class="signal-blue"><p>#00adef</p></div>\n\
+        <div class="dark-signal-blue"><p>#00688f</p></div>\n\
+      </div>\n\
+  	  <div class="swatch-container">\n\
+        <div class="signal-red"><p>#ea1c2c</p></div>\n\
+        <div class="dark-signal-red"><p>#b81623</p></div>\n\
+      </div>\n\
+  	  <div class="swatch-container">\n\
+        <div class="teal"><p>#00a19a</p></div>\n\
+        <div class="dark-teal"><p>#004b48</p></div>\n\
+      </div>\n\
+  	  <div class="swatch-container">\n\
+        <div class="green"><p>#00a10b</p></div>\n\
+        <div class="dark-green"><p>#006107</p></div>\n\
+      </div>\n\
+  	  <div class="swatch-container">\n\
+        <div class="blue-lagoon"><p>#004d6b</p></div>\n\
+        <div class="dark-blue-lagoon"><p>#00364a</p></div>\n\
+      </div>\n\
+  	  <div class="swatch-container">\n\
+        <div class="lemon-curd"><p>#ffc556</p></div>\n\
+        <div class="dark-lemon-curd"><p>#edb750</p></div>\n\
+      </div>\n\
+  	  <div class="swatch-container">\n\
+        <div class="grey"><p>#cccccc</p></div>\n\
+        <div class="dark-grey"><p>#333333</p></div>\n\
+      </div>\n\
+  	  <div class="swatch-container">\n\
+        <div class="white"><p>#ffffff</p></div>\n\
+        <div class="light-grey"><p>#dfdfdf</p></div>\n\
       </div>'
     }
   ]
 };
-
-
-
