5a8b048c58327df21c5cbc609765e107643a4859
Brian Peacock <bpeacock@signal.co>
2014-12-18 22:56:24 -0600

    Made menu work

---
+++ main.js
@@ -0,0 +1,80 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+$(function() {
+	var $window = $(window);
+
+	// Navigation Positioning
+	var $aside 	= $('aside'),
+			headerH = 70,
+			lastScroll;
+
+	var onScroll = function() {
+		var scrollTop = $window.scrollTop();
+
+		if(scrollTop < 0) {
+			$aside.css('top', headerH);
+		}
+		else if(scrollTop < headerH) {
+			$aside.css('top', headerH - scrollTop);
+		}
+		else {
+			$aside.css('top', 0);
+		}
+	};
+
+	$window.scroll(onScroll);
+	onScroll();
+
+	// Navigation Sizing
+	var asideHeight = $aside.height();
+
+	var onResize = function() {
+		$aside.outerHeight($window.height());
+	};
+
+	$window.resize(onResize);
+	onResize();
+
+	// Active Link Selection
+	var path 				= window.location.pathname,
+			$headings   = $('h2[id]'),
+			$asideLinks = $('aside a');
+
+	if(path === '/') {
+		var onScroll = function() {
+			// Get the current heading
+			var hash,
+					scrollTop = $window.scrollTop();
+
+			$headings.each(function() {
+				var $this = $(this),
+						top 	= $this.position().top;
+
+				if(scrollTop > top) {
+					hash = $(this).attr('id');
+				}
+			});
+
+			// Highlight the link
+			$asideLinks.removeClass('active');
+			$('aside a[href="#'+hash+'"]').addClass('active');
+		};
+
+		$window.scroll(onScroll);
+
+		onScroll();
+	}
+	else {
+		var regex = new RegExp('^[^#].*'+path+'?$');
+
+		$('aside a').each(function() {
+			var $this = $(this),
+					href 	= $this.attr('href');
+
+			if(regex.test(href)) {
+				$this.addClass('active');
+			}
+		});
+	}
+});
+
+},{}]},{},[1])
