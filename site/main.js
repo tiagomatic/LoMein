aa98dea71145fbe58b03987159362086bdc866cb
Brian Peacock <bpeacock@signal.co>
2014-12-18 22:38:09 -0600

    Sidebar positioning

--- site/main.js
+++ site/main.js
@@ -1,18 +1,83 @@
 $(function() {
-	var regex = new RegExp(window.location.pathname+'?$');
+	var $window = $(window);
 
+	// Navigation Positioning
+	var $aside 	= $('aside'),
+			headerH = 70;
 
-	console.log(regex);
+	var onScroll = function() {
+		var scrollTop = $window.scrollTop();
 
-	$('aside a').each(function() {
-		var $this = $(this),
-				href 	= $this.attr('href');
-
-		console.log(href);
-
-		if(regex.test(href)) {
-			console.log('hi');
-			$this.addClass('active');
+		if(scrollTop < 0) {
+			$aside.css('top', headerH);
 		}
-	});
+		else if(scrollTop < headerH) {
+			$aside.css('top', headerH - scrollTop);
+		}
+		else {
+			$aside.css('top', 0);
+		}
+	};
+
+	$window.scroll(onScroll);
+	onScroll();
+
+	// Navigation Sizing
+	var asideHeight = $aside.height();
+
+	var onResize = function() {
+		var windowH = $window.height();
+
+		if(windowH > asideHeight) {
+			$aside.height(windowH);
+		}
+		else {
+			$aside.css('height', 'auto');
+		}
+	};
+
+	$window.resize(onResize);
+	onResize();
+
+	// Active Link Selection
+	var path 				= window.location.pathname,
+			$headings   = $('h2[id]'),
+			$asideLinks = $('aside a');
+
+	if(path === '/') {
+		var onScroll = function() {
+			// Get the current heading
+			var hash,
+					scrollTop = $window.scrollTop();
+
+			$headings.each(function() {
+				var $this = $(this),
+						top 	= $this.position().top;
+
+				if(scrollTop > top) {
+					hash = $(this).attr('id');
+				}
+			});
+
+			// Highlight the link
+			$asideLinks.removeClass('active');
+			$('aside a[href="#'+hash+'"]').addClass('active');
+		};
+
+		$window.scroll(onScroll);
+
+		onScroll();
+	}
+	else {
+		var regex = new RegExp('^[^#].*'+path+'?$');
+
+		$('aside a').each(function() {
+			var $this = $(this),
+					href 	= $this.attr('href');
+
+			if(regex.test(href)) {
+				$this.addClass('active');
+			}
+		});
+	}
 });
--- styles/docs.js
+++ styles/docs.js
@@ -7,7 +7,6 @@ module.exports = {
     'tag',
     'button',
     'input',
-    'list',
-    'example'
+    'list'
   ]
 };
